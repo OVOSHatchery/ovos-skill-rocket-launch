f08aa9cd226206e9cb091f52d398bb84be31d2d0
jarbasal <jarbasai@mailfence.com>
2020-06-30 02:31:56 +0100

    gui

--- README.md
+++ README.md
@@ -1,17 +1,25 @@
-## Space Launch
-rocket launch dates
+# <img src='./icon.png' width='50' height='50' style='vertical-align:bottom'/> Rocket Launches
 
-## Description
+## About
+
 This skill interacts with https://launchlibrary.net/ api to return the latest space launch.
 
+![](gui.png)
+
 ## Examples
-* "next rocket launch"
-* "i want to know more"
+* "when is the next rocket launch"
+* "tell me more"
+
+## TODO
+* next/previous intent
 
 ## Credits
+- JarbasAi
+- [msev](https://github.com/marksev1/Mycroft-SpaceLaunch-Skill)
+- [praxeo](https://github.com/praxeo/mycroft_spaceflightnow_skill)
 
-msev
+## Category
+**Information**
 
-JarbasAI
-
-[praxeo](https://github.com/praxeo/mycroft_spaceflightnow_skill)
+## Tags
+#space #information #gui
--- __init__.py
+++ __init__.py
@@ -1,58 +1,64 @@
 from adapt.intent import IntentBuilder
 from mycroft.skills.core import MycroftSkill, intent_handler
-
 import requests
-import arrow
-
-__author__ = 'msev', 'jarbas'
+from datetime import datetime, timedelta
+from lingua_franca.format import nice_date, nice_duration
 
 
 class SpaceLaunchSkill(MycroftSkill):
-    def __init__(self):
-        super(SpaceLaunchSkill, self).__init__(name="SpaceLaunchSkill")
-
-    @intent_handler(IntentBuilder("SpaceLaunchIntent").\
-            require("SpaceLaunchKeyword").optionally(
-            'ExactLaunchKeyword').optionally("next"))
+    # https://www.rocketlaunch.live/api
+    # https://launchlibrary.net/
+    @intent_handler(IntentBuilder("SpaceLaunchIntent")
+                    .require("SpaceLaunchKeyword")
+                    .optionally('ExactLaunchKeyword').optionally("next"))
     def handle_space_launch_intent(self, message):
         try:
-            r = requests.get("https://launchlibrary.net/1.2/launch/next/1")
-            date = str(r.json()['launches'][0]['windowstart'])
-            description = str(r.json()['launches'][0]["missions"][0]
-                                   ["description"])
-            self.set_context("launch_description", description)
-            day_len = len(date.split(",")[0].split(" ")[1])
-
-            if day_len == 1 and 'ExactLaunchKeyword' in message.data:
-                date_time = 'at ' + arrow.get(date, 'MMMM D, YYYY HH:mm:SS')\
-                    .to('local').format('HH:mm, on MMMM DD, YYYY')
-            elif day_len == 1:
-                date_time = arrow.get(date, 'MMMM D, YYYY HH:mm:SS')\
-                    .to('local').humanize()
-            elif 'ExactLaunchKeyword' in message.data:
-                date_time = 'at ' + arrow.get(date, 'MMMM DD, YYYY HH:mm:SS')\
-                    .to('local').format('HH:mm, on MMMM DD, YYYY')
+            r = requests.get("https://launchlibrary.net/1.2/launch/next/1"
+                             "").json()
+            dt = datetime.strptime(r['launches'][0]["windowstart"],
+                                   "%B %d, %Y %H:%M:%S UTC")
+            image = r['launches'][0]["rocket"]["imageURL"]
+            description = str(r['launches'][0]["missions"][0]["description"])
+            rocket = str(r['launches'][0]['rocket']['name'])
+            location = str(r['launches'][0]['location']['pads'][0]['name'])
+            now = datetime.now()
+            delta = dt - now
+            if delta <= timedelta(days=2):
+                date_time = nice_duration(delta, lang=self.lang, speech=True)
+                self.speak_dialog("space.launch.delta",
+                                  data={'rocket': rocket,
+                                        'delta': date_time,
+                                        'location': location})
             else:
-                date_time = arrow.get(date, 'MMMM DD, YYYY HH:mm:SS').to(
-                        'local').humanize()
+                date_time = nice_date(dt, lang=self.lang, now=now)
+                self.speak_dialog("space.launch",
+                                  data={'rocket': rocket,
+                                        'time': date_time,
+                                        'location': location})
 
-            rocket = str(r.json()['launches'][0]['rocket']['name'])
-            location = str(r.json()['launches'][0]['location']['pads'][0]
-                           ['name'])
-            self.speak_dialog("space.launch",
-                              data={'rocket': rocket,
-                                    'time': date_time,
-                                    'location': location})
+            self.gui.show_image(image, caption=location,
+                                title=rocket, override_idle=True,
+                                fill='PreserveAspectFit',)
+
+            self.set_context("launch_description", description)
+            self.set_context("rocketPic", image)
+            self.set_context("rocket", rocket)
+
         except Exception as e:
             self.log.error(e)
             self.speak_dialog("not.found")
 
-    @intent_handler(IntentBuilder("SpaceLaunchIntent")
-                    .require("launch_description")
-                    .require('MoreKeyword'))
-    def handle_space_launch_intent(self, message):
+    @intent_handler(IntentBuilder("SpaceLaunchMoreIntent")
+                    .require("launch_description").require("rocket")
+                    .require("rocketPic").require('MoreKeyword'))
+    def handle_space_launch_desc_intent(self, message):
         description = message.data["launch_description"]
+        rocket = message.data["rocket"]
+        image = message.data["rocketPic"]
         self.speak(description)
+        self.gui.show_image(image, caption=description,
+                            title=rocket, override_idle=True,
+                            fill='PreserveAspectFit', )
 
 
 def create_skill():
